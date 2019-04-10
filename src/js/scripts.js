d93aade5f9dcbd38b201d4501739d02ca176fae5
lattrellsapon <lattrellsapon@gmail.com>
2019-04-10 17:38:44 +1200

    Added new items

---
+++ src/js/scripts.js
@@ -0,0 +1,18 @@
+// const modal = document.getElementById('modal');
+// const cokeButton = document.getElementById('coke-button');
+
+// const closeButton = document.getElementById('close');
+
+// cokeButton.onclick = () => {
+//     modal.style.display = 'block';
+// };
+
+// closeButton.onclick = () => {
+//     modal.style.display = 'none';
+// };
+
+// window.onclick = (event) => {
+//     if(event.target == modal){
+//         modal.style.display = 'none';
+//     }
+// };
